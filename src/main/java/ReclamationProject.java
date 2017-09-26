dc6fd110842cda4bc52d698f128fa467b7c010b9
edmorales <edmorales@LAPTOP-7VDQ1BFI>
2017-09-26 16:34:53 -0500

    Fixed all the spacing errors. Tried to understand what that last chunk of for-loops accomplish but I got lost in the sauce. Renamed a few variables. Still needs work, but I wanted to work on my MP2 instead. Priorities.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,26 +1,31 @@
-/*
+/*ct
  * The following code needs a lot of TLC. So give it some!
  *
  * 1. Fix all checkstyle errors
  * 2. Determine what it does (it's going to be tough before you finish #1)
- * 3. Improve the name of the methods and variables
+ * 3. Improve the name of the methjeods and variables
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+public class ReclamationProject {
+    static String doit(String shorter, String longer) {
+        if (shorter.length() > longer.length()) {
+            String swapper = shorter;
+            shorter = longer;
+            longer = swapper; // longer is now the larger string and shorter is the shorter String
+            }
+        String r = ""; // Empty String
         /*
-         * For loop with i
+         * Look at this mess below.
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < shorter.length(); i++) {
+            for (int j = shorter.length() - i; j > 0; j--) {
+                for (int k = 0; k < longer.length() - j; k++) {
+                    r = (shorter.regionMatches(i, longer, k, j) && j > r.length())
+                            ? shorter.substring(i, i + j) : r;
+                        }
+                }
+        }
+        return r; }
 }
